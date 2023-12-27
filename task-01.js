4f87c4717405a50f4effeca48695ec02271478ac
Aleksandr Shabanov <shabalexx@yandex.ru>
2023-12-27 20:48:16 +0300

    first part

---
+++ task-01.js
@@ -0,0 +1,5 @@
+const counter1 = {};
+const counter2 = new Object();
+const counter3 = Object.create({});
+const counter4 = Object.assign({});
+const counter5 = Object.fromEntries([]);
---
+++ task-02.js
@@ -0,0 +1,3 @@
+const objCopy1 = { ...obj };
+const objCopy2 = Object.assign({}, obj);
+const objCopy3 = Object.fromEntries(Object.entries(obj));
